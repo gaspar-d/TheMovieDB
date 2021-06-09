b4ed6459c9336a52cb35aaefae60433a20d11cd1
Diogo Gaspar <ghostlabz@gmail.com>
2021-06-09 19:24:46 -0300

    Initial commit

---
+++ TheMovieDB_App/Resources/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  TheMovieDB_App
+//
+//  Created by Diogo Gaspar on 07/06/21.
+//
+
+import Foundation
+
+struct Constants {
+	let APIKey = "Vai dar erro por que eu tirei a chave, mas é só pegar uma no site se quiser testar, é grátis"
+	let baseImageURL = "https://image.tmdb.org/t/p/original"
+	let baseAPIUrl = "https://api.themoviedb.org/3/movie/now_playing?&language=pt-BR&api_key="
+}
